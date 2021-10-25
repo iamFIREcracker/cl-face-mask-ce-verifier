0e323478f2b79525e7a3df401bc1afed6deca722
Matteo Landi <matteo@matteolandi.net>
2021-10-25 10:36:10 +0200

    Fix running on Replit
    - Don't need to use `nix-shell` anymore
    - Require 'ASDF before everything else
    - Entries pushed inside `ASDF:*CENTRAL-REGISTRY*` need to point to
      directories, so made sure of that -- by adding a trailing `/`
    - Simplified replit.lisp -- all the setup is happening directly inside
      .replit

--- .replit-files/setup.lisp
+++ .replit-files/setup.lisp
@@ -1,8 +1,6 @@
-(load "init.lisp")
-
-(pushnew '(merge-pathnames (parse-namestring ".replit-files/swank-tunnel")
+(pushnew '(merge-pathnames (parse-namestring ".replit-files/swank-tunnel/")
            *default-pathname-defaults*)
          asdf:*central-registry*)
-(pushnew '(merge-pathnames (parse-namestring ".replit-files/ngrok")
+(pushnew '(merge-pathnames (parse-namestring ".replit-files/ngrok/")
            *default-pathname-defaults*)
          asdf:*central-registry*)
--- build/replit.lisp
+++ build/replit.lisp
@@ -1,6 +1,3 @@
-(load "setup.lisp")
-(load "../.replit-files/setup.lisp")
-
 (ql:quickload '("face-mask-ce-verifier-web" "swank-tunnel"))
 
 (fmcv:start :web-interface "0.0.0.0")
